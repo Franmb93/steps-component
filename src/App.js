19f56ad4fd5729dc95c9d90c2cdb7b2c1b5f2364
Francisco Muñoz Betanzos <fcomunozbetanzos@gmail.com>
2023-06-10 10:56:39 +0200

    [Fix/Readme] Small fixes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>React App</h1>
     </div>
   );
 }
-
-export default App;
